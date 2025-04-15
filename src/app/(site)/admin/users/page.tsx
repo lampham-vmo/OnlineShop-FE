36a89f46271af0946465e190b4f150da64fa5dc2
Anh <anhthh3@vmogroup.com>
2025-04-15 09:32:49 +0700

    Fix html nested

---
+++ src/app/(site)/admin/users/page.tsx
@@ -0,0 +1,11 @@
+`use client`
+import ManageAccount from '@/app/components/ManageAccount'
+import React from 'react'
+
+export default function ManageAccountPage() {
+  return (
+    <div>
+      <ManageAccount />
+    </div>
+  )
+}
