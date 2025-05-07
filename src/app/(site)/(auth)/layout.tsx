2e0001eb429f8f96da916d755afaff807f54e119
Nguyen Phu Quan <quannp3@vmogroup.com>
2025-05-07 15:56:06 +0700

    get cart when login

--- src/app/(site)/(auth)/layout.tsx
+++ src/app/(site)/(auth)/layout.tsx
@@ -4,6 +4,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 
 import { useAuthStore } from '@/stores/authStore';
 import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import useCartStore from '@/stores/useCartStore';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -20,20 +22,54 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = useAuthStore((state) => state.user);
+  const [hasHydrated, setHasHydrated] = useState(false);
 
-  if (user) {
-    window.location.href = '/';
-    return null;
-  }
+  const authPersist = useAuthStore.persist;
+  const cartPersist = useCartStore.persist;
+
+  useEffect(() => {
+    let authHydrated = authPersist.hasHydrated();
+    let cartHydrated = cartPersist.hasHydrated();
+    const checkAllHydrated = () => {
+      if (authHydrated && cartHydrated) {
+        setHasHydrated(true);
+      }
+    };
+
+    if (!authHydrated || !cartHydrated) {
+      const unsubAuth = authPersist.onHydrate(() => {
+        authHydrated = true;
+        checkAllHydrated();
+      });
+
+      const unsubCart = cartPersist.onHydrate(() => {
+        cartHydrated = true;
+        checkAllHydrated();
+      });
+
+      authPersist.rehydrate();
+      cartPersist.rehydrate();
+
+      return () => {
+        unsubAuth();
+        unsubCart();
+      };
+    } else {
+      setHasHydrated(true);
+    }
+  }, []);
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster />
+        {hasHydrated ? (
+          <>
+            {children}
+            <Toaster />
+          </>
+        ) : null}
       </body>
     </html>
   );
